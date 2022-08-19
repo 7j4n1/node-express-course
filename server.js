6b1a7e0352ce399459c49f909323b2d6ea7f0194
7j4n1 <ahmaholy@gmail.com>
2022-08-19 01:12:52 +0100

    add login POST Route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const mockData = [
     {name:'Mark'},
@@ -21,6 +23,27 @@ app.get('/users/:id',function(req, res) {
         message: 'got one user.',
         user: req.params.id
     })
+})
+
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username===mockUsername && password===mockPassword) {
+        res.json({
+            success: true,
+            message: 'username and password match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'username and password do not match.'
+        })
+    }
 })
 
 app.listen(8000,function(){
